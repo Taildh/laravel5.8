6b56632bd9afc4427b1c17ab9a734ad92808c2f3
taildh <ledinhhuutaii@gmail.com>
2019-10-14 17:05:33 +0700

    first commit

---
+++ database/migrations/2019_10_13_092758_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->float('price', 20, 3);
+            $table->string('image', 255);  
+            $table->text('description');
+            $table->integer('quantity');   
+            $table->integer('views');   
+            $table->integer('status');   
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
